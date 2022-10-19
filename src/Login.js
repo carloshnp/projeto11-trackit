efc429960c7351e5949d1b396b1efb52b1c09e43
carloshnp <carloshnpwork@gmail.com>
2022-10-19 17:49:39 -0300

    componentes da tela de login e de cadastro adicionados

--- src/Login.js
+++ src/Login.js
@@ -1,25 +1,44 @@
 import styled from "styled-components"
 import { Link } from "react-router-dom"
+import logo from "./img/Logo.png"
 
 export default function Login() {
     return (
         <Container>
-            <Logo alt='logo' />
+            <Logo src={logo} alt='logo' />
             <Form>
-                Formulário
+                <input placeholder="email" />
+                <input placeholder="senha" />
+                <button>Entrar</button>
             </Form>
             <Link to={`/cadastro`}>
-                Cadastro
+                Não tem uma conta? Cadastre-se!
             </Link>
         </Container>
     )
 }
 
 const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 `
 
 const Logo = styled.img`
+    width: 180px;
 `
 
 const Form = styled.form`
+    width: 200px;
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+
+    input {
+        margin-bottom: 20px;
+    }
+
+    button {
+        margin-bottom: 20px;
+    }
 `
